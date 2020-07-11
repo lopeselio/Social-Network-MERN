b6f76e2b82814a4805d8737f8652173aa098689c
lopeselio <lopeselio@gmail.com>
2020-07-12 01:04:28 +0530

    commit

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const app = express()
 const PORT = 5000
+// OdqYehjgy6cY0kpS
 
-app.get('/', (req, res) => {
-    res.send('hello world')
-})
+// const customMiddleware = (req, res, next) =>{
+//     console.log('middleware executed!')
+//     next()
+// }
+
+// app.use(customMiddleware)
+
+// app.get('/', (req, res) => {
+//     console.log('home')
+//     res.send('hello world')
+// })
 
 app.listen(PORT, () => {
     console.log('Server is connected via', PORT)
