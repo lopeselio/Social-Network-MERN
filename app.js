1c2f1cc2e1b658f09aab9944ac9e4caf3a9104e3
lopeselio <lopeselio@gmail.com>
2020-07-12 01:12:50 +0530

    connected mongo

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 const PORT = 5000
+const {MONGOURI} = require('./keys')
+
+mongoose.connect(MONGOURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+mongoose.connection.on('connected', ()=>{
+    console.log('connected to mongo')
+})
+mongoose.connection.on('error', (err)=>{
+    console.log('error connecting',err)
+})
+
+
+
 // OdqYehjgy6cY0kpS
 
 // const customMiddleware = (req, res, next) =>{
