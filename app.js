28e0e87c22b3f71d1d54cac5eab3ce3b6d245d49
lopeselio <lopeselio@gmail.com>
2020-07-16 01:54:42 +0530

    refactor

--- app.js
+++ app.js
@@ -3,11 +3,7 @@ const app = express()
 const mongoose = require('mongoose')
 const PORT = 5000
 const {MONGOURI} = require('./keys')
-require('./models/user')
-app.use(express.json())
 
-
-app.use(require('./routes/auth'))
 
 mongoose.connect(MONGOURI, {
     useNewUrlParser: true,
@@ -19,6 +15,9 @@ mongoose.connection.on('connected', ()=>{
 mongoose.connection.on('error', (err)=>{
     console.log('error connecting',err)
 })
+require('./models/user')
+app.use(express.json())
+app.use(require('./routes/auth'))
 
 app.listen(PORT, () => {
     console.log('Server is connected via', PORT)
