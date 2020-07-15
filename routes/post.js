10fd40c8592e8895628e12e2dfe73a0724341c8a
lopeselio <lopeselio@gmail.com>
2020-07-16 02:27:49 +0530

    tweak

--- routes/post.js
+++ routes/post.js
@@ -1,6 +1,29 @@
 const express = require('express')
-const app = express()
+const router = express.Router()
 const mongoose = require('mongoose')
+const requireLogin = require('../middleware/requireLogin')
+const Post =  mongoose.model("Post")
+
+router.post('/createpost',requireLogin,(req,res) => {
+    const {title, body} = req.body
+    if(!title || !body){
+        return req.status(422).json({error: 'Please add all the fields'})
+    }
+    // console.log(req.user)
+    // res.send(('ok'))
+    req.user.password = undefined
+    const post = new Post({
+        title,
+        body,
+        postedBy: req.user
+    })
+    post.save().then(result=>{
+        res.json({post:result})
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+})
 
 
 module.exports = router
