e2daefe467a40c01f3be10dfa91261195c277589
lopeselio <lopeselio@gmail.com>
2020-07-16 01:22:45 +0530

    bcrypt jsonwebtoken

--- routes/auth.js
+++ routes/auth.js
@@ -3,38 +3,68 @@ const router = express.Router()
 const mongoose = require('mongoose')
 const User = mongoose.model("User")
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const {JWT_SECRET} = require('../keys')
 
 router.get('/',(req, res) => {
     res.send('hello')
 })
 
-router.post('/signup', (req, res) =>{
-    // console.log(req.body)
-    const {name, email, password} = req.body
+router.post('/signup',(req,res)=>{
+    const {name,email,password,pic} = req.body 
     if(!email || !password || !name){
-        return res.status(422).json({error: 'please add all the fields'})
+       return res.status(422).json({error:"please add all the fields"})
     }
     User.findOne({email:email})
     .then((savedUser)=>{
         if(savedUser){
-            return res.status(422).json({error:"user already exists with that email"})
+          return res.status(422).json({error:"user already exists with that email"})
         }
-        const user = new User({
-            email,
-            password,
-            name,
+        bcrypt.hash(password,12)
+        .then(hashedpassword=>{
+              const user = new User({
+                  email,
+                  password:hashedpassword,
+                  name,
+              })
+      
+              user.save()
+              .then(user=>{
+                  res.json({message:"saved successfully"})
+              })
+              .catch(err=>{
+                  console.log(err)
+              })
         })
-        user.save()
-        .then(user=>{
-            res.json({message: "saved successfully"})
+       
+    })
+    .catch(err=>{
+      console.log(err)
+    })
+})
+router.post('/signin',(req,res)=>{
+    const {email,password} = req.body
+    if(!email || !password){
+       return res.status(422).json({error:"please add email or password"})
+    }
+    User.findOne({email:email})
+    .then(savedUser=>{
+        if(!savedUser){
+           return res.status(422).json({error:"Invalid Email or password"})
+        }
+        bcrypt.compare(password,savedUser.password)
+        .then(doMatch=>{
+            if(doMatch){
+                const token = jwt.sign({_id:savedUser._id},JWT_SECRET)
+                res.json({token})
+            }
+            else{
+                return res.status(422).json({error:"Invalid Email or password"})
+            }
         })
-        .catch(err => {
+        .catch(err=>{
             console.log(err)
         })
-        // res.json({message: 'successfully posted'})
-    })
-    .catch(err => {
-        console.log(err)
     })
 })
 
