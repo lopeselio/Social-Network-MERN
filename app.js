3fa8feeed2098bb4262fdc7da95c16e632b8ca44
lopeselio <lopeselio@gmail.com>
2020-07-16 00:34:49 +0530

    hashing the password

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const mongoose = require('mongoose')
 const PORT = 5000
 const {MONGOURI} = require('./keys')
 require('./models/user')
+app.use(express.json())
 
-app.use(require('./models/routes/auth'))
+
+app.use(require('./routes/auth'))
 
 mongoose.connect(MONGOURI, {
     useNewUrlParser: true,
@@ -17,22 +19,6 @@ mongoose.connection.on('connected', ()=>{
 mongoose.connection.on('error', (err)=>{
     console.log('error connecting',err)
 })
-
-
-
-// OdqYehjgy6cY0kpS
-
-// const customMiddleware = (req, res, next) =>{
-//     console.log('middleware executed!')
-//     next()
-// }
-
-// app.use(customMiddleware)
-
-// app.get('/', (req, res) => {
-//     console.log('home')
-//     res.send('hello world')
-// })
 
 app.listen(PORT, () => {
     console.log('Server is connected via', PORT)
--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,41 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
+const User = mongoose.model("User")
+const bcrypt = require('bcryptjs')
 
 router.get('/',(req, res) => {
     res.send('hello')
 })
 
 router.post('/signup', (req, res) =>{
-    console.log(req.body.name)
+    // console.log(req.body)
+    const {name, email, password} = req.body
+    if(!email || !password || !name){
+        return res.status(422).json({error: 'please add all the fields'})
+    }
+    User.findOne({email:email})
+    .then((savedUser)=>{
+        if(savedUser){
+            return res.status(422).json({error:"user already exists with that email"})
+        }
+        const user = new User({
+            email,
+            password,
+            name,
+        })
+        user.save()
+        .then(user=>{
+            res.json({message: "saved successfully"})
+        })
+        .catch(err => {
+            console.log(err)
+        })
+        // res.json({message: 'successfully posted'})
+    })
+    .catch(err => {
+        console.log(err)
+    })
 })
 
 module.exports = router
