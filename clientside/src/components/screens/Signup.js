67c524ecbe9fb6aab205b4ffc6ad4e3dcb703be9
lopeselio <lopeselio@gmail.com>
2020-07-18 05:11:05 +0530

    sign up

--- clientside/src/components/screens/Signup.js
+++ clientside/src/components/screens/Signup.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Signup = () => {
     return (
-        <h1>Signup</h1>
+        <div className="mycard">
+            <div className="card authcard input-field">
+                <h2>Social Network</h2>
+                <input type="text" placeholder="name" />
+                <input type="email" placeholder="email" />
+                <input type="password" placeholder="password" />
+                <button className="btn waves-effect waves-light #7b1fa2 purple darken-2">Signup</button>
+            </div>
+        </div>
     )
 }
 
