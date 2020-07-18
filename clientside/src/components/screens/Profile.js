535e186d44fbcc429bd6a03f109c0323f5b143b7
lopeselio <lopeselio@gmail.com>
2020-07-18 06:27:43 +0530

    profile ictures

--- clientside/src/components/screens/Profile.js
+++ clientside/src/components/screens/Profile.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Profile = () => {
     return (
-        <div>
+        <div style={{
+            maxWidth:"550px", 
+            margin: "0px auto"
+        }}>
             <div style={{
                 display: "flex",
                 margin: "18px 0px",
