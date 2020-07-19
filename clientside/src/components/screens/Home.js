44d2db2aa268b2dca3fcd08786db5418c17a51be
lopeselio <lopeselio@gmail.com>
2020-07-19 18:22:45 +0530

    added home screen

--- clientside/src/components/screens/Home.js
+++ clientside/src/components/screens/Home.js
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <h1>Home</h1>
+        <div className="home">
+            <h5>Elio</h5>
+            <div className="card-image">
+                <img src="https://images.unsplash.com/photo-1568194157720-8bbe7114ebe8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+            </div>
+            <div className="card-content">
+                <h6>Title</h6>
+                <p>This is an amazing post</p>
+                <input type="text" placeholder="add a comment" />
+            </div>
+        </div>
     )
 }
 
