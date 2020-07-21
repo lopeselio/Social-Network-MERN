d4c8fd0a98f5278a9efd1829b1318af68d840be8
lopeselio <lopeselio@gmail.com>
2020-07-21 16:08:20 +0530

    added cards and mat-icon

--- clientside/src/components/screens/Home.js
+++ clientside/src/components/screens/Home.js
@@ -3,14 +3,54 @@ import React from 'react'
 const Home = () => {
     return (
         <div className="home">
-            <h5>Elio</h5>
-            <div className="card-image">
-                <img src="https://images.unsplash.com/photo-1568194157720-8bbe7114ebe8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+            <div className="card home-card">
+                <h5>Elio</h5>
+                <div className="card-image">
+                    <img src="https://images.unsplash.com/photo-1568194157720-8bbe7114ebe8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+                </div>
+                <div className="card-content">
+                    <i class="material-icons" style={{color:"red"}}>favorite</i>
+                    <h6>Title</h6>
+                    <p>This is an amazing post</p>
+                    <input type="text" placeholder="add a comment" />
+                </div>
             </div>
-            <div className="card-content">
-                <h6>Title</h6>
-                <p>This is an amazing post</p>
-                <input type="text" placeholder="add a comment" />
+            <div className="card home-card">
+                <h5>Elio</h5>
+                <div className="card-image">
+                    <img src="https://images.unsplash.com/photo-1568194157720-8bbe7114ebe8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+                </div>
+                <div className="card-content">
+                    <i class="material-icons" style={{color:"red"}}>favorite</i>
+
+                    <h6>Title</h6>
+                    <p>This is an amazing post</p>
+                    <input type="text" placeholder="add a comment" />
+                </div>
+            </div>
+            <div className="card home-card">
+                <h5>Elio</h5>
+                <div className="card-image">
+                    <img src="https://images.unsplash.com/photo-1568194157720-8bbe7114ebe8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+                </div>
+                <div className="card-content">
+                    <i class="material-icons" style={{color:"red"}}>favorite</i>
+                    <h6>Title</h6>
+                    <p>This is an amazing post</p>
+                    <input type="text" placeholder="add a comment" />
+                </div>
+            </div>
+            <div className="card home-card">
+                <h5>Elio</h5>
+                <div className="card-image">
+                    <img src="https://images.unsplash.com/photo-1568194157720-8bbe7114ebe8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+                </div>
+                <div className="card-content">
+                    <i class="material-icons" style={{color:"red"}}>favorite</i>
+                    <h6>Title</h6>
+                    <p>This is an amazing post</p>
+                    <input type="text" placeholder="add a comment" />
+                </div>
             </div>
         </div>
     )
