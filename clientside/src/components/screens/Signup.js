ffc70d396316e28c53caa07df8493062eaf83bc4
lopeselio <lopeselio@gmail.com>
2020-07-21 17:31:11 +0530

    changes

--- clientside/src/components/screens/Signup.js
+++ clientside/src/components/screens/Signup.js
@@ -1,10 +1,31 @@
 import React,{ useState } from 'react'
 import {Link} from 'react-router-dom'
+import M from 'materialize-css'
 
 const Signup = () => {
+    const fetch = require("node-fetch")
     const [name, setName] = useState("")
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const postData = () => {
+        fetch("/signup",{
+            method: "post",
+            headers: {
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify({
+                name: "",
+                email: "",
+                password: ""
+            })
+        }).then(res => res.json())
+        .then(data => {
+            if(data.err){
+                M.toast({html: data.err})
+
+            }
+        })
+    }
 
     return (
         <div className="mycard">
@@ -13,7 +34,7 @@ const Signup = () => {
                 <input type="text" placeholder="name" value={name} onChange={(e) => setName(e.target.value)} />
                 <input type="email" placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} />
                 <input type="password" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                <button className="btn waves-effect waves-light #7b1fa2 purple darken-2">Signup</button>
+                <button className="btn waves-effect waves-light #7b1fa2 purple darken-2" onClick={()=> postData()}>Signup</button>
                 <h5>
                     <Link to="/login">Already have an Account?</Link>
                 </h5>
